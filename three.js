8059802b1cbf6ec86c23e0ceab6652309b9469bc
ritik155 <112944907+ritik155@users.noreply.github.com>
2022-10-11 12:06:09 +0530

    okkk

---
+++ three.js
@@ -0,0 +1,5 @@
+let arr = [10, 20]
+arr.unshift()
+console.log(arr);
+arr.shift()
+console.log(arr);
---
+++ two.js
@@ -0,0 +1,5 @@
+let size = ['s', 'm', 'l', 'xl']
+console.log(size);
+let removed_ele = size.pop()
+console.log(size);
+console.log(removde_ele);
